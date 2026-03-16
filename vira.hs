765c76941e08b497f0b0472aa9d91cae811df4bc
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2026-03-16 15:34:20 -0400

    Move /test directory under /nix (#10)
    ## Summary
    
    - Move `/test` to `/nix/test` to keep top-level clean
    - Update `skills.url` path from `..` to `../..`
    - Update vira.hs build config to reference new location
    
    Closes #7

--- vira.hs
+++ vira.hs
@@ -2,7 +2,7 @@
 \ctx pipeline ->
   pipeline
     { signoff.enable = True
-    , build.flakes = [".", "./test" { overrideInputs = [("skills", ".")] }]
+    , build.flakes = [".", "./nix/test" { overrideInputs = [("skills", ".")] }]
     , build.systems = ["x86_64-linux", "aarch64-darwin"]
     , cache.url = Nothing  -- TODO: configure Attic cache URL
     }
